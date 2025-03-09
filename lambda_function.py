110d507eed451f9d651fd6e17652e1d0b6b024aa
john costello <jcostello711@gmail.com>
2025-03-08 21:03:13 -0500

    updating lambda to extract slider values and seed count

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,18 @@ import json
 
 def lambda_handler(event, context):
     
+    slider1 = event.get("slider1")
+    slider2 = event.get("slider2")
+    slider3 = event.get("slider3")
+    seedCount = event.get("seedCount")
+
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
-        "body": json.dumps({"message": "Hello, world!", "status": "success"})
+        "body": json.dumps(
+            {
+                "message": f"Extracted values: Slider 1 is {slider1}, Slider 2 is {slider2}, Slider 3 is {slider3}, Number of seeds requested {seedCount}", 
+                "status": "success"
+            }
+        )
     }
